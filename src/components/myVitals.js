e71ec6d7fe722ecdf81274c42ea421b52e08c21c
Sev Perez <severinperez@gmail.com>
2018-01-07 10:58:56 +0000

    refactor vitals calculations

--- src/components/myVitals.js
+++ src/components/myVitals.js
@@ -15,53 +15,67 @@ const MyVitals = (props) => {
     );
   }
   
+  // Collect run data
+  let runData = [];
+  _.each(Object.keys(props.runs), key => { runData.push(props.runs[key])});
+  
   // Longest Run
   const dateOptions = { year: "numeric", month: "long", day: "numeric" };
-  const maxDistanceKey = _.max(Object.keys(props.runs), key => {
-    return props.runs[key].distance;
+  const maxDistanceRun = _.max(runData, function(run) {
+    return run.distance;
   });
-  const maxDistance = (props.runs[maxDistanceKey].distance).toFixed(2);
-  const maxDistanceDate = new Date(props.runs[maxDistanceKey].date).toLocaleDateString("en-US", dateOptions);
+  const maxDistance = maxDistanceRun.distance;
+  const maxDistanceDate = new Date(maxDistanceRun.date).toLocaleDateString("en-US", dateOptions);
   
   // Record Pace
-  const recordPaceKey = _.min(Object.keys(props.runs), key => {
-    return props.runs[key].seconds / props.runs[key].distance;
+  const recordPaceRun = _.min(runData, function(run) {
+    return run.seconds / run.distance;
   });
-  const recordPace = props.runs[recordPaceKey].seconds / props.runs[recordPaceKey].distance;
+  const recordPace = recordPaceRun.seconds / recordPaceRun.distance;
   const recordPaceMin = Math.floor(recordPace / 60);
   const recordPaceSec = String(Math.floor(recordPace % 60)).padStart(2, "0");
-  const recordPaceDate = new Date(props.runs[recordPaceKey].date).toLocaleDateString("en-US", dateOptions);
+  const recordPaceDate = new Date(recordPaceRun.date).toLocaleDateString("en-US", dateOptions);
   
   // Total Time
-  const allSeconds = _.map(Object.keys(props.runs), key => { 
-    return props.runs[key].seconds;
-  });
-  const totalSeconds = allSeconds.reduce(function(sum, val) { 
-    return sum += val;
+  const totalSeconds = _.reduce(runData, function(sum, run) {
+    return sum + run.seconds;
   }, 0);
   const totalMin = Math.floor(totalSeconds / 60);
   const timeHr = Math.floor(totalMin / 60);
-  const timeMin = String(totalMin % 60).padStart(2, "0");
-  const timeSec = String(totalSeconds % 60).padStart(2, "0");
-  const firstRunKey = _.min(Object.keys(props.runs), key => {
-    return new Date(props.runs[key].date);
+  const timeMin = totalMin % 60;
+  const timeSec = totalSeconds % 60;
+  
+  // First Run
+  const firstRun = _.min(runData, run => {
+    return new Date(run.date);
   });
-  const firstRunDate = new Date(props.runs[firstRunKey].date).toLocaleDateString("en-US", dateOptions);
+  const firstRunDate = new Date(firstRun.date).toLocaleDateString("en-US", dateOptions);
   
   // Total Distance
-  const allMiles = _.map(Object.keys(props.runs), key => { 
-    return props.runs[key].distance;
-  });
-  const totalMiles = allMiles.reduce(function(sum, val) {
-    return sum += val;
+  const totalMiles = _.reduce(runData, function(sum, run) {
+    return sum + run.distance;
   }, 0);
+  
+  // Average Pace
+  const avgPace = totalSeconds / totalMiles;
+  const avgPaceMin = Math.floor(avgPace / 60);
+  const avgPaceSec = String(Math.floor(avgPace % 60)).padStart(2, "0");
   
   return (
     <div className="row border border-top-0 border-right-0 border-left-0 p-3 pb-4 mb-3">
       <h3>Vitals</h3>
       <div className="container">
         <div className="row">
-          <div className="col-3">
+          <div className="col-4">
+            <dl>
+              <dt>Total Runs</dt>
+              <dd>
+                <p className="mb-0">{runData.length} runs</p>
+                <p className="mb-0 text-secondary"><small>Since {firstRunDate}</small></p>
+              </dd>
+            </dl>
+          </div>
+          <div className="col-4">
             <dl>
               <dt>Total Distance:</dt>
               <dd>
@@ -70,7 +84,7 @@ const MyVitals = (props) => {
               </dd>
             </dl>
           </div>
-          <div className="col-3">
+          <div className="col-4">
             <dl>
               <dt>Total Time:</dt>
               <dd>
@@ -79,7 +93,7 @@ const MyVitals = (props) => {
               </dd>
             </dl>
           </div>
-          <div className="col-3">
+          <div className="col-4">
             <dl>
               <dt>Longest Run:</dt>
               <dd>
@@ -88,7 +102,16 @@ const MyVitals = (props) => {
               </dd>
             </dl>
           </div>
-          <div className="col-3">
+          <div className="col-4">
+            <dl>
+              <dt>Average Pace:</dt>
+              <dd>
+                <p className="mb-0">{`${avgPaceMin}:${avgPaceSec} min/mile`}</p>
+                <p className="mb-0 text-secondary"><small>Since {firstRunDate}</small></p>
+              </dd>
+            </dl>
+          </div>
+          <div className="col-4">
             <dl>
               <dt>Record Pace:</dt>
               <dd>
